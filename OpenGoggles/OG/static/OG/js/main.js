3a7db9e82043099e0ff3fe5ecafacb5eed3e7417
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2020-12-26 00:34:43 +0330

    Improved front-end side

--- OpenGoggles/OG/static/OG/js/main.js
+++ OpenGoggles/OG/static/OG/js/main.js
@@ -29,5 +29,18 @@ function search_phrase()
 
 function process_result(result) {
     result = JSON.parse(result)
-    alert(result.Released)
+    
+    document.getElementById('Poster').src = result.Poster
+    document.getElementById('Title').innerHTML = " <b>" + result.Title + "</b>"
+    document.getElementById('Year').innerHTML = " <b>" + result.Year + "</b>"
+    document.getElementById('Genre').innerHTML = " <b>" + result.Genre + "</b>"
+    document.getElementById('Duration').innerHTML = " <b>" + result.Duration + "</b>"
+    document.getElementById('Director').innerHTML = " <b>" + result.Director + "</b>"
+    document.getElementById('Actors').innerHTML = " <b>" + result.Actors + "</b>"
+    document.getElementById('Awards').innerHTML = " <b>" + result.Awards + "</b>"
+    document.getElementById('Plot').innerHTML = " <b>" + result.Title + "</b>"
+    document.getElementById('IMDB').innerHTML = result.Ratings[0].Value
+    document.getElementById('RottenTomatoes').innerHTML = result.Ratings[1].Value
+    document.getElementById('Metacritic').innerHTML = result.Ratings[2].Value
+
 }
